b23da85db21f300c03ab85318dd7461ed4cf94d7
Amit Singh <abcdamit44@gmail.com>
2022-03-28 20:08:54 +0530

    Check

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,15 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState();
   const showText = () => {
-    setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+    setText(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
   }
   return (
     <div id="main">
       {/* // Do not alter the main div */}
       <button id="click" onClick={showText}>Click</button>
-      <p id="para">{text}</p>
+      {text}
     </div>
   );
 }
